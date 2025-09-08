d82a620255aa12a15fcd5c36d8642781e344a06e
AERE_06 <ramosalbert909@gmail.com>
2025-09-07 20:15:44 -0600

    Add files via upload

---
+++ ADA2- Gestión de Memoria en Python/MemoriaDinamica.py
@@ -0,0 +1,11 @@
+frutas = []
+frutas.append("Mango")
+frutas.append("Manzana")
+frutas.append("Banana")     
+frutas.append("uvas")
+print(frutas)
+
+frutas.remove("Mango")
+frutas.remove("Manzana")
+frutas.append("Sandía")
+print(frutas)
